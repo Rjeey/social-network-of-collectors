fae566b3a6bdef7bb0762ac64fb36b8b255fa7ee
Rjeey <licanomega@gmail.com>
2020-08-18 23:42:25 +0300

    first commit

---
+++ spring-boot-react-private-collection-manager/backend-private-collection-manager/src/test/java/by/rjeey/PrivatecollectionManager/PrivatecollectionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.rjeey.PrivatecollectionManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivatecollectionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
