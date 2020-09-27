5333e6e5c07d13e25df350ed30f668de5cf4fa7f
Artem Malchihin <licanomega@gmail.com>
2020-09-27 20:05:35 +0300

    new front

--- src/main/frontend/src/component/Home/Home.js
+++ src/main/frontend/src/component/Home/Home.js
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from "react";
 import Register from "../registration/registration";
 import UserService from "../../service/user.service";
 import "./Home.css";
+import AuthService from "../../service/auth.service";
 
 const Home = () => {
+
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
   return (
     <div className="container">
       <div className="row mt-5">
@@ -11,9 +22,13 @@ const Home = () => {
           <h1 className="h1 font-weight-bold">Sing up Right now</h1>
           <h6 className="mb-4 spacing">Project Collections Manager</h6>
         </div>
-        <div className="col-md-6 col-xl-5 md-4 ">
-          <Register />
-        </div>
+        {!currentUser &&
+            (
+            <div className="col-md-6 col-xl-5 md-4 ">
+              <Register/>
+            </div>
+            )
+        }
       </div>
     </div>
   );
