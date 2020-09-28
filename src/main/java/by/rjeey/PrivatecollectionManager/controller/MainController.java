4ec2b9d3e419d3c8965984ea2a535c80ebca43fa
Artem Malchihin <licanomega@gmail.com>
2020-09-28 14:50:30 +0300

    change get index

--- src/main/java/by/rjeey/PrivatecollectionManager/controller/MainController.java
+++ src/main/java/by/rjeey/PrivatecollectionManager/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
@@ -13,9 +14,8 @@ public class MainController {
     @Value("${server.servlet.context-path}")
     private String contextPath;
 
-    @GetMapping("*")
-    public ModelAndView defaultPage(Map<String, Object> model) {
-        model.put("contextPath", contextPath);
-        return new ModelAndView("index", model);
+    @GetMapping(value ={"/", ""})
+    public String getIndex(HttpServletRequest request) {
+        return "src/main/front/public/index.html";
     }
 }
